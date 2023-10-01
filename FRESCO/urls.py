d2a724ae65ce8112d5e142ebda4fc54c4acaaaf3
Josh <jmckerra@purdue.edu>
2023-10-01 13:11:26 -0600

    Website updates

--- FRESCO/urls.py
+++ FRESCO/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='FRESCO-Data-Repo-Home'),
-    path('about/', views.about, name='FRESCO-Data-Repo-About')
+    path('about/', views.about, name='FRESCO-Data-Repo-About'),
 ]
