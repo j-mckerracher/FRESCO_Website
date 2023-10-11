6767a381617951263583857c7c3b02c22e09a53e
Josh <jmckerra@purdue.edu>
2023-10-11 15:07:07 -0600

    Pushing progress.

--- FRESCO/urls.py
+++ FRESCO/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='FRESCO-Data-Repo-Home'),
     path('about/', views.about, name='FRESCO-Data-Repo-About'),
+    path('team/', views.team, name='FRESCO-Data-Repo-Team'),
+    path('news/', views.news, name='FRESCO-Data-Repo-News')
 ]
--- FRESCO/views.py
+++ FRESCO/views.py
@@ -11,3 +11,13 @@ def home(request):
 def about(request):
     template = f'{template_dir}about.html'
     return render(request, template)
+
+
+def team(request):
+    template = f'{template_dir}team.html'
+    return render(request, template)
+
+
+def news(request):
+    template = f'{template_dir}news.html'
+    return render(request, template)
