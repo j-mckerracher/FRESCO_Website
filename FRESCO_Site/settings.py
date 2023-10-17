917175b9c9fedc2e7b666e8d2160bad24454e837
Josh <jmckerra@purdue.edu>
2023-10-17 16:21:08 -0600

    Vercel updates

--- FRESCO_Site/settings.py
+++ FRESCO_Site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'frescodata.xyz']
 
 
 # Application definition
