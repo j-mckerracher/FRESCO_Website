2dfab9a05e192c6becfddee19189e2bf550893b6
Josh <jmckerra@purdue.edu>
2023-10-18 12:20:26 -0600

    Redeploying

--- FRESCO_Site/settings.py
+++ FRESCO_Site/settings.py
@@ -82,6 +82,12 @@ DATABASES = {
     }
 }
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'FRESCO/static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
