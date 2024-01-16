ca5a9803726026e2b32bb36f9c785bf6e64042ce
Josh <jmckerra@purdue.edu>
2024-01-16 12:04:33 -0700

    Added load static

--- FRESCO_Site/settings.py
+++ FRESCO_Site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.xyz']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.xyz', 'localhost']
 
 
 # Application definition
