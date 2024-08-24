464c2d413c22e7855bb5c5ef9502a4c81b2ea29f
Josh <jmckerra@purdue.edu>
2024-08-24 09:37:09 -0600

    logging update

--- FRESCO/views_helpers.py
+++ FRESCO/views_helpers.py
@@ -13,18 +13,18 @@ logger = logging.getLogger(__name__)
 
 
 def get_authorization():
-    logger.info("Starting authorization process")
+    logger.warning("Starting authorization process")
 
     username = "api_user"  # os.environ["API_USER"]
     password = os.environ["API_USER_AUTH"]  # Example password
     data = {"username": username, "password": password}
     url = os.environ["AUTH_URL"]
 
-    logger.info("Sending authorization request to %s", url)
+    logger.warning("Sending authorization request to %s", url)
     response = retry_request(url, get_or_post="post", data=data, max_retries=MAX_RETRIES, initial_delay=INITIAL_DELAY)
 
     if response:
-        logger.info("Authorization successful")
+        logger.warning("Authorization successful")
         return response.json()["access_token"]
     else:
         logger.error("Encountered an error getting API authorization")
@@ -55,7 +55,7 @@ def send_simple_search_request(form_data: str, search_type: str):
     :return: response (dict or str): A dictionary containing the JSON response from the request if successful,
     or a failure message string if the request fails or the authorization token is not obtained.
     """
-    logger.info("Sending simple search request for form data: %s, search type: %s", form_data, search_type)
+    logger.warning("Sending simple search request for form data: %s, search type: %s", form_data, search_type)
 
     global MAX_RETRIES, INITIAL_DELAY
     # jwt = get_authorization()
@@ -63,11 +63,11 @@ def send_simple_search_request(form_data: str, search_type: str):
     #     logger.error("Failed to get API authorization")
     #     return "Failed to get API authorization!"
 
-    # logger.info("Received JWT for authorization")
+    # logger.warning("Received JWT for authorization")
     # headers = {"Authorization": f"Bearer {jwt}"}
 
     url = os.getenv("BASE_API_ADDRESS")
-    logger.info("Constructed URL for request: %s", url)
+    logger.warning("Constructed URL for request: %s", url)
 
     # Prepare the data to be sent in the body of the POST request
     data = {
@@ -88,7 +88,7 @@ def send_simple_search_request(form_data: str, search_type: str):
         logger.warning("Received string response, indicating a possible error: %s", response)
         return response
     elif response is not None and response.json():
-        logger.info("Received successful JSON response")
+        logger.warning("Received successful JSON response")
         return response.json()
     else:
         logger.error("Couldn't connect to the Database or empty response")
@@ -105,11 +105,11 @@ def make_post_request(url, data):
 
     :return: response: The response object returned by the requests library.
     """
-    logger.info("Making POST request to %s", url)
+    logger.warning("Making POST request to %s", url)
 
     try:
         response = requests.post(url, data=data)
-        logger.info("POST request to %s successful", url)
+        logger.warning("POST request to %s successful", url)
         return response
     except requests.RequestException as e:
         logger.error("Post request failed: %s", e)
@@ -126,11 +126,11 @@ def make_get_request(url, headers):
     :return: response (requests.Response or None): The response object from the request, or None if the request fails
     due to an exception.
     """
-    logger.info("Making GET request to %s", url)
+    logger.warning("Making GET request to %s", url)
 
     try:
         response = requests.get(url, headers=headers)
-        logger.info("GET request to %s successful", url)
+        logger.warning("GET request to %s successful", url)
         return response
     except requests.RequestException as e:
         logger.error("Request failed: %s", e)
@@ -192,20 +192,20 @@ def identify_input_type_host_search(input_str: str):
     :return: str: Returns 'job' if the input string matches the job identifier pattern, 'node' if it matches the node
     identifier pattern, or 'unknown' if it matches neither.
     """
-    logger.info("Identifying input type for host search: %s", input_str)
+    logger.warning("Identifying input type for host search: %s", input_str)
 
     job_id_pattern = r"^JOB\d+$"
     node_id_pattern = r"^NODE\d+$"
 
     # Check if the input matches any of the patterns
     if re.match(job_id_pattern, input_str):
-        logger.info("Input identified as job ID")
+        logger.warning("Input identified as job ID")
         return "job"
     elif re.match(node_id_pattern, input_str):
-        logger.info("Input identified as node ID")
+        logger.warning("Input identified as node ID")
         return "node"
     else:
-        logger.info("Input type is unknown")
+        logger.warning("Input type is unknown")
         return "unknown"
 
 
@@ -221,12 +221,12 @@ def identify_input_type_job_search(input_str: str) -> str:
     :return: str: Returns the type of job search or job status ('group', 'job', 'user', 'jobname', 'node',
     specific statuses, or 'unknown').
     """
-    logger.info("Identifying input type for job search: %s", input_str)
+    logger.warning("Identifying input type for job search: %s", input_str)
 
     # Specific job status values
     valid_statuses = {'CANCELLED', 'COMPLETED', 'FAILED', 'NODE_FAIL', 'TIMEOUT'}
     if input_str in valid_statuses:
-        logger.info("Input identified as a job exit status")
+        logger.warning("Input identified as a job exit status")
         return 'exit_code'
 
     # Check for prefixes with digits
@@ -236,10 +236,10 @@ def identify_input_type_job_search(input_str: str) -> str:
             # Extract the numerical part and check if it's all digits
             prefix_length = len(prefix)
             if input_str[prefix_length:].isdigit():
-                logger.info(f"Input identified as {prefix.lower()}")
+                logger.warning(f"Input identified as {prefix.lower()}")
                 return prefix.lower()  # Return the prefix type in lowercase
 
-    logger.info("Input type is unknown")
+    logger.warning("Input type is unknown")
     return "unknown"
 
 
@@ -254,18 +254,18 @@ def is_valid_host_search(search: str) -> bool:
 
     :return: bool: True if the search string is a valid host search identifier, False otherwise.
     """
-    logger.info("Validating host search identifier: %s", search)
+    logger.warning("Validating host search identifier: %s", search)
 
     if search.startswith("NODE"):
         is_valid = search[4:].isdigit()  # Check if everything after "NODE" is digits
-        logger.info("Validation result for NODE: %s", is_valid)
+        logger.warning("Validation result for NODE: %s", is_valid)
         return is_valid
     elif search.startswith("JOB"):
         is_valid = search[3:].isdigit()  # Check if everything after "JOB" is digits
-        logger.info("Validation result for JOB: %s", is_valid)
+        logger.warning("Validation result for JOB: %s", is_valid)
         return is_valid
 
-    logger.info("Search string does not conform to valid host search identifiers")
+    logger.warning("Search string does not conform to valid host search identifiers")
     return False
 
 
@@ -281,12 +281,12 @@ def is_valid_job_search(search: str) -> bool:
 
     :return: bool: True if the search string is a valid search identifier, False otherwise.
     """
-    logger.info("Validating job search identifier: %s", search)
+    logger.warning("Validating job search identifier: %s", search)
 
     # Check for job status values
     valid_statuses = {'CANCELLED', 'COMPLETED', 'FAILED', 'NODE_FAIL', 'TIMEOUT'}
     if search in valid_statuses:
-        logger.info("Search string is a valid job status value")
+        logger.warning("Search string is a valid job status value")
         return True
 
     # Check for prefixes with digits
@@ -296,10 +296,10 @@ def is_valid_job_search(search: str) -> bool:
             # Correctly extract the numerical part and check if it's all digits
             prefix_length = len(prefix)  # Get the length of the prefix
             is_valid = search[prefix_length:].isdigit()  # Check if the remaining part is all digits
-            logger.info("Validation result for %s: %s", prefix, is_valid)
+            logger.warning("Validation result for %s: %s", prefix, is_valid)
             return is_valid
 
-    logger.info("Search string does not conform to valid job search identifiers")
+    logger.warning("Search string does not conform to valid job search identifiers")
     return False
 
 
@@ -312,13 +312,13 @@ def clean_and_uppercase(input_str: str):
 
     :return: str: The cleaned string, containing only letters and numbers in uppercase format.
     """
-    logger.info("Cleaning and converting to uppercase: %s", input_str)
+    logger.warning("Cleaning and converting to uppercase: %s", input_str)
 
     if input_str is None:
-        logger.info("Input string is None, returning empty string")
+        logger.warning("Input string is None, returning empty string")
         return ""
 
     cleaned_str = ''.join(char for char in input_str if char.isalnum() or char == '_').upper()
-    logger.info("Cleaned and uppercased string: %s", cleaned_str)
+    logger.warning("Cleaned and uppercased string: %s", cleaned_str)
 
     return cleaned_str
